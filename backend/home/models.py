8c7b9a262d06c2f9b9734ee6907326af48680489
Saurav Gharti <sp.gharti@gmail.com>
2022-11-11 20:39:04 +0545

    Update models.py

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,8 +1,15 @@
 from django.conf import settings
 from django.db import models
+import requests
+
 class Hello(models.Model):
     'Generated Model'
     name = models.CharField(max_length=256,)
 class Hi(models.Model):
     'Generated Model'
     name = models.CharField(max_length=256,)
+    
+    def test_method(self):
+        response = requests.get("https://hello.com")
+        data = response.json() 
+        return data
